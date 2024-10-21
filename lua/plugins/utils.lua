eeae8c344d44ba05d4a7e2f136db2dabc72543c9
RVSmooth <itzsubhashis@proton.me>
2024-10-21 16:13:09 +0530

    Autohide barbar

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -67,9 +67,11 @@ return {
 		'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
 		'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
 	},
-	init = function() vim.g.barbar_auto_setup = false end,
+	init = function() 
+		vim.g.barbar_auto_setup = false 
+	end,
 	opts = {
-
+		auto_hide = true
 	},
 	version = '^1.0.0', -- optional: only update when a new 1.x version is released
 },
