67213d8d12b6c8262ee2944c366ab98a985a365c
RVSmooth <riveeks.smooth@gmail.com>
2024-11-17 20:39:22 +0530

    chore: Add remaps for window height and width control

--- lua/personal/remap.lua
+++ lua/personal/remap.lua
@@ -21,4 +21,8 @@ vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { silent = true })
 vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { silent = true })
 vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", { silent = true })
 
-
+-- Resize mappings using Control + hjkl
+vim.api.nvim_set_keymap('n', '<C-k>', ':resize -3<CR>', { noremap = true, silent = true })  -- Resize up
+vim.api.nvim_set_keymap('n', '<C-j>', ':resize +3<CR>', { noremap = true, silent = true })  -- Resize down
+vim.api.nvim_set_keymap('n', '<C-h>', ':vertical resize -3<CR>', { noremap = true, silent = true })  -- Resize left
+vim.api.nvim_set_keymap('n', '<C-l>', ':vertical resize +3<CR>', { noremap = true, silent = true })  -- Resize right
